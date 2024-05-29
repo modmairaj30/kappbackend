7ae12bbc63b9e18806992527bdffb99278a483e9
Mairaj <modmairaj30@gmail.com>
2024-05-29 16:34:39 +0530

    ter

--- kappbackend/src/main/java/com/k/spring/security/constant/CommonConstant.java
+++ kappbackend/src/main/java/com/k/spring/security/constant/CommonConstant.java
@@ -1,13 +1,13 @@
 package com.k.spring.security.constant;
 
-public interface CommonConstant {
-String ERROR_ROLE=	"Error: Role is not found.";
-String ERROR_USERNAME_ALREADY_TAKEN =	"Error: Username is already taken!";
-String ERROR_EMAIL_ALREADY_INUSE =	"Error: Email is already in use!";
-String USER_REGISTERED =	"User registered successfully!";
-String SIGN_OUT = "You've been signed out!";
+public final class CommonConstant {
+	public static final String ERROR_ROLE=	"Error: Role is not found.";
+	public static final String ERROR_USERNAME_ALREADY_TAKEN =	"Error: Username is already taken!";
+	public static final String ERROR_EMAIL_ALREADY_INUSE =	"Error: Email is already in use!";
+	public static final String USER_REGISTERED =	"User registered successfully!";
+	public static final String SIGN_OUT = "You've been signed out!";
 
-
+	private CommonConstant() {}
 
 
 }
